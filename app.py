a8c49c921cabe1f8627fa14268d71bc54b556efd
Niceasvini <viniiciusniceas@gmail.com>
2025-05-31 15:41:31 -0300

    .

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
         logging.StreamHandler(sys.stdout),
-        logging.FileHandler('app.log')
+        logging.FileHandler('app.log', encoding='utf-8')
     ]
 )
 logger = logging.getLogger(__name__)
